863c4ddf28ec02b65d61e4cbf133bae756ba94c1
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-04-14 15:09:50 +0200

    Create file in output directory

--- own/move.py
+++ own/move.py
@@ -3,3 +3,7 @@ def move_files(input_dir: str, output_dir: str):
         raise Exception('Empty input directory')
     if len(output_dir) == 0:
         raise Exception('Empty output directory')
+    create_file(output_dir + 'file.pdf')
+
+def create_file(filename: str) -> None:
+    open(filename, 'w').close()
--- test/move_test.py
+++ test/move_test.py
@@ -6,6 +6,8 @@ from pytest import raises
 
 from own.move import move_files
 
+import os.path
+
 def test_standard():
     move_files('input', 'output')
 
@@ -18,3 +20,12 @@ def test_validate_non_empty_output_directory():
     with raises(Exception) as exp:
         move_files('output', '')
     assert str(exp.value) == 'Empty output directory'
+
+def test_creates_file_in_output_directory():
+    remove_if_exists('../resource/tmp/file.pdf')
+    move_files('irrelevant', '../resource/tmp/')
+    assert os.path.isfile('../resource/tmp/file.pdf')
+
+def remove_if_exists(filename: str) -> None:
+    if os.path.isfile(filename):
+        os.remove(filename)
