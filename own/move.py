9434f05200a004d6a150b33a27af295f980dd5d5
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-04-14 17:21:38 +0200

    Copy two files from input directory to output directory

--- own/move.py
+++ own/move.py
@@ -1,9 +1,15 @@
-def move_files(input_dir: str, output_dir: str):
+def run_program(input_dir: str, output_dir: str, filenames: list[str]) -> None:
     if len(input_dir) == 0:
         raise Exception('Empty input directory')
     if len(output_dir) == 0:
         raise Exception('Empty output directory')
-    create_file(output_dir + 'file.pdf')
 
-def create_file(filename: str) -> None:
-    open(filename, 'w').close()
+    for filename in filenames:
+        with open(input_dir + filename) as file:
+            foo = file.read()
+        create_file(output_dir + filename, foo)
+
+def create_file(filepath: str, content: str) -> None:
+    file = open(filepath, 'w')
+    file.write(content)
+    file.close()
--- test/move_test.py
+++ test/move_test.py
@@ -4,28 +4,57 @@
 
 from pytest import raises
 
-from own.move import move_files
+from own.move import run_program
 
 import os.path
 
 def test_standard():
-    move_files('input', 'output')
+    run_program('input', 'output', [])
 
 def test_validate_non_empty_input_directory():
     with raises(Exception) as exp:
-        move_files('', 'output')
+        run_program('', 'output', [])
     assert str(exp.value) == 'Empty input directory'
 
 def test_validate_non_empty_output_directory():
     with raises(Exception) as exp:
-        move_files('output', '')
+        run_program('output', '', [])
     assert str(exp.value) == 'Empty output directory'
 
-def test_creates_file_in_output_directory():
-    remove_if_exists('../resource/tmp/file.pdf')
-    move_files('irrelevant', '../resource/tmp/')
-    assert os.path.isfile('../resource/tmp/file.pdf')
+def test_copies_file_from_input_directory():
+    create_file("../resource/bankStatements/a.pdf", 'car')
+    run_program("../resource/bankStatements/", "../resource/tmp/", ['a.pdf'])
+    assert read_file("../resource/tmp/a.pdf") == 'car'
+
+def test_copies_two_files_from_input():
+    create_file("../resource/bankStatements/a.pdf", 'car1')
+    create_file("../resource/bankStatements/b.pdf", 'car2')
+    run_program("../resource/bankStatements/", "../resource/tmp/", ['a.pdf', 'b.pdf'])
+    assert read_file("../resource/tmp/b.pdf") == 'car2'
+
+# zastanów się, jaką rzecz nową chcesz dodać do programu
+# (np. kopiowanie plików)
+# napisz test pod to:
+#   - (test) stwórz pusty folder
+#   - (test) wsadź tam jakiś plik
+#   - (test) stwórz drugi pusty folder
+#   - (test) odpal program, podając oba foldery jako parametry
+#   - (test) move_file(): 'swoja logika'
+#   - (test) przejdź tylko wtedy jak w tym drugim katalogu jest plik o takiej
+#            nazwie i takiej treści
+# napisz implementację:
+#   - skopiuj plik, os.path.copy()
 
 def remove_if_exists(filename: str) -> None:
     if os.path.isfile(filename):
         os.remove(filename)
+
+def read_file(filename: str) -> str:
+    with open(filename) as file:
+        return file.read()
+
+def create_file(filename: str, body: str) -> None:
+    if not os.path.isdir(os.path.dirname(filename)):
+        os.mkdir(os.path.dirname(filename))
+    with open(filename, mode='w') as file:
+        file.write(body)
