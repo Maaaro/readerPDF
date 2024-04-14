4d34ddd67124698267df9896f927adf0146b9fa8
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-04-14 13:32:18 +0200

    Clean

--- main.py
+++ main.py
@@ -1,47 +1,27 @@
-# importing all the required modules
 import glob
+
 import PyPDF2
 import pandas as pd
 
-# creating a list with a directions of .pdf
-# def listOfLinks():
-# try:
-sourceList = glob.glob("resource/invoice/*.pdf")
-print("Lista zrodel: ", sourceList)
-# except:
-#     print("Brak plików PDF w katalogu!")
-
-# def listofstringtosearch():
+invoice_filepaths = glob.glob("resource/invoice/*.pdf")
+print("Lista zrodel: ", invoice_filepaths)
 
 df = pd.read_excel(r"resource/test.xlsx", sheet_name="Arkusz1")
-# print(df)
 
 df = df.astype(str)
 list_of_id = df["NR faktury"].to_list()
 
 print(list_of_id)
 
-# def searchingTrougthTheList(sourceList=None):
-iterate = 1
-for i in sourceList:
-    # creating a pdf reader object
+for filepath in invoice_filepaths:
+    reader = PyPDF2.PdfReader(filepath)
 
-    reader = PyPDF2.PdfReader(sourceList[iterate])
-    iterate = iterate + 1
-    # print("reader: ",reader)
-    # print the number of pages in pdf file
-    print("\nLiczba stron: ", len(reader.pages), i)
-
-    # print the text of the first page
+    print("\nLiczba stron: ", len(reader.pages), filepath)
     textFromPDF = reader.pages[0].extract_text()
-    # print(texttosearch)
-
-    # searchString = "PG/1253/3/373/118"
     for id in list_of_id:
         if id in textFromPDF:
             print("\n-------Tak znaleziono-------->  ", id)
             _match = 1
             break
-        else:
-            print("Nie znaleziono ->", id)
-            _match = 0
+        print("Nie znaleziono ->", id)
+        _match = 0
