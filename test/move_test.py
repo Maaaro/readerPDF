497f9c4eb6570526f92668a7d7c4652e9957ca72
Marek <fakeemail@.com>
2024-04-15 23:11:27 +0200

    Rename new file in output directory

--- test/move_test.py
+++ test/move_test.py
@@ -32,6 +32,12 @@ def test_copies_two_files_from_input():
     run_program("../resource/bankStatements/", "../resource/tmp/", ['a.pdf', 'b.pdf'])
     assert read_file("../resource/tmp/b.pdf") == 'car2'
 
+def test_rename_filename_from_output_directory():
+    remove_if_exists("aaa.pdf")
+    create_file("../resource/bankStatements/a.pdf",'car')
+    run_program("../resource/bankStatements/","../resource/tmp/",['a.pdf'])
+    rename_filename("../resource/tmp/","a.pdf","aaa.pdf")
+
 def remove_if_exists(filename: str) -> None:
     if os.path.isfile(filename):
         os.remove(filename)
@@ -45,3 +51,7 @@ def create_file(filename: str, body: str) -> None:
         os.mkdir(os.path.dirname(filename))
     with open(filename, mode='w') as file:
         file.write(body)
+
+def rename_filename(output_dir: str,filename: str,new_filename: str) -> None:
+    if not os.path.isdir(os.path.dirname(output_dir+filename)):
+        os.rename(output_dir+filename,new_filename)
