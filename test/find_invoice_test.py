f0e56dfa1d7f5b1ebdd6d8630cb9f574ba4a0af3
Marek <fakeemail@.com>
2024-04-20 14:18:36 +0200

    find_invoice() returns 'no file found' for none pdf file

--- test/find_invoice_test.py
+++ test/find_invoice_test.py
@@ -2,7 +2,7 @@ import glob
 import os
 
 def find_invoice(invoices_folder: str, invoice_number: str) -> str:
-    invoice_filepaths = glob.glob(invoices_folder + "/*")
+    invoice_filepaths = glob.glob(invoices_folder + "/*.pdf")
     if len(invoice_filepaths) == 0:
         return "no file found"
     return os.path.basename(invoice_filepaths[0])
@@ -12,9 +12,10 @@ def test_one_invoice_one_file():
                                     "FV/2022/08/1253/3/11034")
     assert invoice_filename == "FV_2022_08_1253_3_11034-V1457523.pdf"
 
-# def test_pdf_file():
-#     invoice_filename = find_invoice("None pdf file",
-#                                     "FV/2022/08/1253/3/11034")
+def test_pdf_file():
+    invoice_filename = find_invoice("None pdf file",
+                                    "FV/2022/08/1253/3/11034")
+    assert invoice_filename == "no file found"
 
 def test_empty_folder():
     invoice_filename = find_invoice("empty", "FV/2022/08/1253/3/11034")
