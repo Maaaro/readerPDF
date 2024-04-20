a9b9aee0787845ce3bcaf769b520a6124f56a3ef
Marek <fakeemail@.com>
2024-04-20 15:01:46 +0200

    find_invoice() raises exception for malformed pdf file

--- test/find_invoice_test.py
+++ test/find_invoice_test.py
@@ -1,10 +1,22 @@
 import glob
 import os
 
+import pytest
+from PyPDF2 import PdfReader
+
 def find_invoice(invoices_folder: str, invoice_number: str) -> str:
+
     invoice_filepaths = glob.glob(invoices_folder + "/*.pdf")
+    if len(invoice_number) == 0:
+        raise Exception("empty invoice number")
     if len(invoice_filepaths) == 0:
         return "no file found"
+
+    try:
+        PdfReader(invoice_filepaths[0])
+    except:
+        raise Exception("malformed pdf file")
+
     return os.path.basename(invoice_filepaths[0])
 
 def test_one_invoice_one_file():
@@ -19,4 +31,19 @@ def test_pdf_file():
 
 def test_empty_folder():
     invoice_filename = find_invoice("empty", "FV/2022/08/1253/3/11034")
+    assert invoice_filename == "no file found"
+
+def test_empty_invoice_number():
+    with pytest.raises(Exception) as e:
+        find_invoice("empty", "")
+    assert str(e.value) == "empty invoice number"
+
+def test_malformed_pdf():
+    with pytest.raises(Exception) as e:
+        find_invoice("malformed pdf", "FV/2022/08/1253/3/11034")
+    assert str(e.value) == "malformed pdf file"
+
+def test_no_match():
+    invoice_filename = find_invoice("no match for invoice number",
+                                    "FV/007/00/0000/0/0004")
     assert invoice_filename == "no file found"
