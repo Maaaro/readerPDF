9bbfe20db126aa2b16222f31ea542c0bea4325a5
Marek <fakeemail@.com>
2024-04-21 16:36:23 +0200

    Extract_invoice_number() return 'column not found' if ther is not such column in df

--- test/get_invoice_id_test.py
+++ test/get_invoice_id_test.py
@@ -1,17 +1,16 @@
 import pytest
 
-from own.invoice_id import extract_invoice_id
+from own.invoice_id import extract_invoice_number
 
 def test_no_excel_file():
-    invoice_number = extract_invoice_id(r"C:\Users\mrowk\OneDrive\Desktop\testy\faktury_id.xlsx", "One")
+    invoice_number = extract_invoice_number("None pdf file/*.xlsx", "One", "")
     assert invoice_number == "file not found"
 
+def test_empty_table():
+    invoice_number = extract_invoice_number("get_invoice_id_tests_folder/faktury_id.xlsx", "EmptyTable", "NR faktury")
+    assert invoice_number == "data not found"
 
-
-
-
-
-# def test_one_invoice_id():
-#     invoice_id = find_invoice_id(r"C:\Users\mrowk\OneDrive\Desktop\testy\faktury_id.xlsx", "26908/BR/2023")
-#     assert invoice_id
+def test_empty_invoice_number_column():
+    invoice_number = extract_invoice_number("get_invoice_id_tests_folder/faktury_id.xlsx", "NoSerachColumn", "NR faktury")
+    assert invoice_number == "column not found"
 
