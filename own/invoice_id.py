12fc95357aeda31edf82ba6c2922eda34f8052cf
Marek <fakeemail@.com>
2024-04-21 17:00:38 +0200

    Extract_invoice_number() return list of invoice_number

--- own/invoice_id.py
+++ own/invoice_id.py
@@ -1,32 +1,14 @@
 import pandas as pd
 
-# column_name: str,
-
 def extract_invoice_number(excelpath: str, sheetname: str, invoice_number_columnname: str):
+    try:
+        df = pd.read_excel(excelpath, sheet_name=sheetname)
+        if df.empty:
+            return "data not found"
+        if invoice_number_columnname not in df.columns:
+            return "column not found"
+    except:
+        return "file not found"
+    invoice_number = df[invoice_number_columnname].to_list()
+    return invoice_number
 
-
-        try:
-                df = pd.read_excel(excelpath, sheet_name=sheetname)
-                if df.empty:
-                        return "data not found"
-                if invoice_number_columnname not in df.columns:
-                        return "column not found"
-        except:
-                df = "file not found"
-
-        return df
-
-
-# def test():
-#         df2 = pd.read_excel(r"../test/get_invoice_id_tests_folder/faktury_id.xlsx", "Empty")
-#         df3 = pd.read_excel(r"../test/get_invoice_id_tests_folder/faktury_id.xlsx", "One")
-#         print("df2 :" + df2)
-#         df2 = df2.empty
-#         print("df2 " + str(df2))
-#         print("df3 :" + df3)
-#         df3 = df3.empty
-#         print("df3 " + str(df3))
-#
-#
-#         df = pd.read_excel(r"../test/get_invoice_id_tests_folder/faktury_id.xlsx", "Empty")
-#         print(df)
--- test/get_invoice_id_test.py
+++ test/get_invoice_id_test.py
@@ -1,5 +1,3 @@
-import pytest
-
 from own.invoice_id import extract_invoice_number
 
 def test_no_excel_file():
@@ -11,6 +9,14 @@ def test_empty_table():
     assert invoice_number == "data not found"
 
 def test_empty_invoice_number_column():
-    invoice_number = extract_invoice_number("get_invoice_id_tests_folder/faktury_id.xlsx", "NoSerachColumn", "NR faktury")
+    invoice_number = extract_invoice_number("get_invoice_id_tests_folder/faktury_id.xlsx", "NoSerachColumn",
+                                            "NR faktury")
     assert invoice_number == "column not found"
 
+def test_one_invoice_number():
+    invoice_number = extract_invoice_number("get_invoice_id_tests_folder/faktury_id.xlsx", "One", "NR faktury")
+    assert invoice_number == ["26908/BR/2023"]
+
+def test_two_invoice_number():
+    invoice_number = extract_invoice_number("get_invoice_id_tests_folder/faktury_id.xlsx", "Two", "NR faktury")
+    assert invoice_number == ["26908/BR/2023", "PL3654810710"]
