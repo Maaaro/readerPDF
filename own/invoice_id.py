153b08d3544a009ec99d32dfbaf224feb0cf7b45
Marek <fakeemail@.com>
2024-04-26 21:27:31 +0200

    new_filenames() return list of invoice new filename

--- own/invoice_id.py
+++ own/invoice_id.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from numpy._core.multiarray import item
 
 def invoice_numbers(excelpath: str, sheetname: str, invoice_number_columnname: str):
     try:
@@ -12,3 +13,17 @@ def invoice_numbers(excelpath: str, sheetname: str, invoice_number_columnname: s
     invoice_numbers = df[invoice_number_columnname].to_list()
     return invoice_numbers
 
+def new_filenames(excelpath: str, sheetname: str, new_filename_column: str):
+    try:
+        df = pd.read_excel(excelpath, sheet_name=sheetname)
+        if df.empty:
+            return "data not found"
+        if new_filename_column not in df.columns:
+            return "column not found"
+    except:
+        return "file not found"
+
+    new_filename = df[new_filename_column].to_list()
+    new_filename = [str(int(i)) + ". fv" for i in new_filename]
+
+    return new_filename
