18837759e4f191bb16277865a4ed1cf9482b330c
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-06-05 23:58:52 +0200

    Remove commented code

--- own/move.py
+++ own/move.py
@@ -1,21 +1,17 @@
 import codecs
 
-
 def run_program(invoices_folder: str, invoice_found: str, output_dir: str, filenames: str) -> None:
     if len(invoices_folder) == 0:
         raise Exception('Empty input directory')
     if len(output_dir) == 0:
         raise Exception('Empty output directory')
 
-    # for i, filename in enumerate(filenames):
     with codecs.open(invoice_found, encoding="latin-1") as file:
         foo = file.read()
         create_file(output_dir + str(filenames), foo)
         file.close()
 
-
 def create_file(filepath: str, content: str) -> None:
     with codecs.open(filepath, encoding="latin-1", mode="w+") as file:
         file.write(content)
         file.close()
-
