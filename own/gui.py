077086a6a720375ca0d602c9f2ce47e46d9ee1b3
Marek Mrówka <M.Mrowka@napollo.pl>
2024-11-25 10:02:07 +0100

    Frezed progress bar was fixed

--- own/gui.py
+++ own/gui.py
@@ -1,5 +1,6 @@
-import tkinter
-
+import threading
+import tkinter as tk
+import time
 from tkinter import *
 from tkinter import ttk, messagebox, filedialog
 from own.find_invoices import find_invoice, pdf_files
@@ -21,8 +22,22 @@ def move_files(invoice_folder: str, output_dir: str, excelpath: str, fileprefix:
 
         error_comment_invoice_list = ["malformed pdf file", "empty invoice number", "no file found"]
         index = 0
+        #popup, progress, progress_step, progress_var = show_progressbar(len(list_of_invoices))
 
-        popup, progress, progress_step, progress_var = show_progressbar(list_of_invoices)
+        progress_window = tk.Toplevel(frame)
+        progress_window.title("Postęp")
+        progress_bar = ttk.Progressbar(progress_window, orient="horizontal", length=200, mode="determinate")
+        progress_bar.pack(pady=20)
+        progress_bar["maximum"] = len(list_of_invoices)
+
+        # Wyśrodkuj okno Toplevel względem root
+        root_x = frame.winfo_x()
+        root_y = frame.winfo_y()
+        root_width = frame.winfo_width()
+        root_height = frame.winfo_height()
+        progress_window.geometry(f"+{root_x + root_width // 2 - 150}+{root_y + root_height // 2 - 50}")
+
+
         status_invoice_list = []
         for (invoice, newfilename, wf_number) in zip(list_of_invoices, list_of_newfilenames, wf_cases):
 
@@ -54,12 +69,23 @@ def move_files(invoice_folder: str, output_dir: str, excelpath: str, fileprefix:
                             newfilename_with_number = newfilename_with_number + str(i) + '.pdf'
                             copy_pdf_file(invoice_folder, match, output_dir, newfilename_with_number)
                 status_invoice_list.append(status_multiple_invoice_list)
-            update_progressbar(popup, progress, progress_step, progress_var)
+
+            #threading.Timer(0.1, update_progressbar, args=(popup, progress, progress_step, progress_var)).start()
+            #update_progressbar(popup, progress, progress_step, progress_var)
+            #gui.root.after(100,update_progressbar, popup, progress, progress_step, progress_var)
+            #gui.root.after(100, gui.root.update_progressbar, popup, progress, progress_step, progress_var)
+            progress_window.update()
+            time.sleep(0.1)
+            progress_bar["value"] += 1
+
+
 
             index = index + 1
 
         add_comment(excelpath, "Sheet1", status_invoice_list, fileprefix)
-        destroy_progressbar(popup)
+        #destroy_progressbar(popup)
+        progress_window.destroy()
+        #gui.root.after(100, gui.root.destroy_progressbar, popup)
         if "ok" in status_invoice_list:
             gui_meseges(1)
         else:
@@ -90,6 +116,7 @@ def gui_meseges(message: int):
                                message="Folder źródłowy jest pusty.")
 
 
+
 def destroy_progressbar(popup):
     popup.destroy()
 
@@ -98,18 +125,20 @@ def update_progressbar(popup, progress: int, progress_step: int, progress_var: D
     popup.update()
     progress += progress_step
     progress_var.set(progress)
+    root.update_idletasks()
 
 
-def show_progressbar(list_of_invoices: list[str]):
-    popup = tkinter.Toplevel()
+def show_progressbar(list_of_invoices: int):
+    popup = tk.Toplevel()
     popup.title("Progress bar")
     progress = 0
     ttk.Label(popup, text="          Wyszukiwanie    ").grid(row=0, column=0)
-    progress_var = tkinter.DoubleVar()
-    progress_bar = ttk.Progressbar(popup, variable=progress_var, maximum=len(list_of_invoices))
+    progress_var = tk.DoubleVar()
+    progress_bar = ttk.Progressbar(popup, variable=progress_var, maximum=list_of_invoices)
     progress_bar.grid(row=1, column=0)
     popup.pack_slaves()
     progress_step = int(1)
+
     return popup, progress, progress_step, progress_var
 
 
@@ -141,31 +170,31 @@ def get_invoices(excelpath: str):
 
 
 def gui():
+    global root, frame
     root = Tk()
-    root.geometry("800x325")
-    Grid.rowconfigure(root, 0, weight=1)
-    Grid.columnconfigure(root, 0, weight=1)
-
     root.title("Program do przenoszenia faktur i wyciągów bankowych")
+    root.geometry("800x325")
+    root.rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
 
     textframe = ttk.Frame(root, padding=1)
-    Grid.columnconfigure(textframe, 0, weight=1)
-    Grid.rowconfigure(textframe, 0, weight=1)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
     textframe.grid(column=0, row=0, sticky="NSEW")
 
     frame = ttk.Frame(root, padding=15)
     frame.grid(column=0, row=1, sticky="NSEW")
-    Grid.rowconfigure(frame, 1, weight=1)
-    Grid.rowconfigure(frame, 2, weight=1)
-    Grid.rowconfigure(frame, 3, weight=1)
-    Grid.rowconfigure(frame, 4, weight=1)
-    Grid.rowconfigure(frame, 5, weight=1)
-    Grid.rowconfigure(frame, 6, weight=1)
-    Grid.rowconfigure(frame, 7, weight=1)
+    frame.rowconfigure(1, weight=1)
+    frame.rowconfigure(2, weight=1)
+    frame.rowconfigure(3, weight=1)
+    frame.rowconfigure(4, weight=1)
+    frame.rowconfigure(5, weight=1)
+    frame.rowconfigure(6, weight=1)
+    frame.rowconfigure(7, weight=1)
 
-    Grid.columnconfigure(frame, 0, weight=1)
-    Grid.columnconfigure(frame, 1, weight=1)
-    Grid.columnconfigure(frame, 2, weight=1)
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(1, weight=1)
+    frame.columnconfigure(2, weight=1)
 
     text = (
             "\nPlik Excel, z którego mają zostać pobrane dane musi zawierać zakładkę 'Sheet1', która zawiera kolumny: \n" +
@@ -175,17 +204,17 @@ def gui():
     ttk.Label(textframe, text=text).grid(column=0, row=0, sticky="NSEW", padx=5, pady=5)
 
     ttk.Label(frame, text="Folder z fakturami / WB").grid(column=0, row=2, sticky="W", padx=5, pady=5)
-    invoices_folder = tkinter.StringVar()
+    invoices_folder = tk.StringVar()
     invoices_folder_entry = ttk.Entry(frame, textvariable=invoices_folder, state="disable")
     invoices_folder_entry.grid(column=1, row=2, sticky="NSEW", padx=5, pady=5)
 
     ttk.Label(frame, text="Folder docelowy").grid(column=0, row=3, sticky="W", padx=5, pady=5)
-    output_dir = tkinter.StringVar()
+    output_dir = tk.StringVar()
     output_dir_entry = ttk.Entry(frame, textvariable=output_dir, state="disable")
     output_dir_entry.grid(column=1, row=3, sticky="NSEW", padx=5, pady=5)
 
     ttk.Label(frame, text="Plik Excel z fakturami").grid(column=0, row=4, sticky="W", padx=5, pady=5)
-    excelpath = tkinter.StringVar()
+    excelpath = tk.StringVar()
     excelpath_entry = ttk.Entry(frame, textvariable=excelpath, state="disable")
     excelpath_entry.grid(column=1, row=4, sticky="NSEW", padx=5, pady=5)
 
@@ -219,7 +248,7 @@ def gui():
             messagebox.showinfo(title="Uwaga", message="Wybierz 'Szukaj faktur' lub 'wyciągów bankowych'")
 
     def file_to_search(option: str):
-        tkinter.Tk().withdraw()
+        tk.Tk().withdraw()
 
         if option == "Invoices" or option == "Output":
             folder_path_string = filedialog.askdirectory()
@@ -254,6 +283,7 @@ def gui():
             excelpath_entry.delete(0, END)
             excelpath_entry.insert(0, path)
             excelpath_entry.config(state="disable")
+
 
     root.mainloop()
 
