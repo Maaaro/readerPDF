9fc1513f4f5f8e622ea9974efbcfccf724f78217
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-11-09 13:02:34 +0100

    Add a failing test to include found case numbers

--- test/domain/test_domain.py
+++ test/domain/test_domain.py
@@ -3,7 +3,6 @@ from fnc.case import Case
 from fnc.domain import which_files_to_move, InvoiceSearchMode
 from test.project_path import project_path
 
-
 def test_for_single_invoice_saves_invoice_only_with_case_prefix():
     files_to_move = which_files_to_move(
         cases=[Case('eIC155687424', '420', None)],
@@ -13,7 +12,6 @@ def test_for_single_invoice_saves_invoice_only_with_case_prefix():
     assert files_to_move == {
         project_path('domain/fixture/single_invoice/eic_155687424.pdf'): 'output/420.pdf'
     }
-
 
 def test_for_multiple_invoices_saves_file_with_case_prefix_and_suffix():
     files_to_move = which_files_to_move(
@@ -26,7 +24,6 @@ def test_for_multiple_invoices_saves_file_with_case_prefix_and_suffix():
         project_path('domain/fixture/multiple_invoices/eic_155687424_copy.pdf'): 'output/42-2.pdf',
     }
 
-
 def test_search_in_subfolder_with_name_of_workflow_number():
     files_to_move = which_files_to_move(
         cases=[Case('eIC155687424', '420', "wf1")],
@@ -36,3 +33,15 @@ def test_search_in_subfolder_with_name_of_workflow_number():
     assert files_to_move == {
         project_path('domain/fixture/with_wf_number/wf1/eic_155687424.pdf'): 'output/420.pdf'
     }
+
+def test_include_invoice_number_for_found_files():
+    files_to_move = which_files_to_move(
+        cases=[
+            Case('eIC155687424', '420', "wf1")
+        ],
+        mode=InvoiceSearchMode.BY_WORKFLOW_NUMBER,
+        source=project_path('domain/fixture/with_wf_number'),
+        target='output/'
+    )
+    assert files_to_move == {'domain/fixture/with_wf_number/wf1/eic_155687424.pdf': 'output/420.pdf'}
+    assert found_case_numbers == ['eIC155687424']
