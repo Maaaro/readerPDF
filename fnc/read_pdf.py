d805019f1ae7f110330a9f2d0702fd73f60a62b5
Maaaro <fakeemail@fakeemail.com>
2026-02-25 19:16:18 +0100

    Handling case with to many char in pdf_path in read_pdf_content().

--- fnc/read_pdf.py
+++ fnc/read_pdf.py
@@ -2,8 +2,13 @@ import pymupdf
 
 
 def read_pdf_content(pdf_path: str) -> str:
-    with pymupdf.open(pdf_path) as pdf_file:
-        content = ''
-        for page in pdf_file:
-            content += page.get_text().strip()
-        return content
+    try:
+        with pymupdf.open(pdf_path) as pdf_file:
+            content = ''
+            for page in pdf_file:
+                content += page.get_text().strip()
+            return content
+    except Exception as e:
+            print(f"Failed to read PDF: {pdf_path}")
+            print(f"Error: {str(e)}")
+            return ""
